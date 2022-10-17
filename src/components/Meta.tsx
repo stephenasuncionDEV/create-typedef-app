3b845eebb36b94ca2d44e94e1612f080cf8b154e
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-10-16 17:57:40 -0700

    ✨ NEW: updated meta

---
+++ src/components/Meta.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+import Head from "next/head";
+
+export interface MetaProps {
+  title: string;
+}
+
+const Meta: FC<MetaProps> = ({ title }) => {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="title" content={title} />
+      <meta name="description" content="DESCRIPTION" />
+      <meta name="keywords" content="KEYWORDS" />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="en" />
+      <meta name="theme-color" content="#348CD4" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="SITE_URL" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content="DESCRIPTION" />
+      <meta property="og:image" content="/assets/logo.png" />
+      <meta property="og:site_name" content="SITE_NAME" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="SITE_URL" />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content="DESCRIPTION" />
+      <meta property="twitter:image" content="/assets/logo.png" />
+    </Head>
+  );
+};
+
+export default Meta;
