faa222accb42944c7f2013f8b30928f45fc68ff1
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-10-25 17:42:46 -0800

    🚀 UPDATE: updated index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
 import type { NextPage } from "next";
-import { Flex, Link } from "@chakra-ui/react";
+import NextImage from "next/image";
+import NextLink from "next/link";
+import { Flex } from "@chakra-ui/react";
 import Meta from "@/components/Meta";
+import ImageLogo from "../public/assets/images/logo.png";
 
 const Home: NextPage = () => {
   return (
     <Flex as="main" flexDir="column">
       <Meta title="Website" />
-      <Link
+      <NextImage src={ImageLogo} alt="Logo" placeholder="blur" />
+      <NextLink
         href="https://github.com/stephenasuncionDEV/next-js-template"
-        isExternal
-        variant="link"
+        target="_blank"
       >
         Github Repository
-      </Link>
+      </NextLink>
     </Flex>
   );
 };
