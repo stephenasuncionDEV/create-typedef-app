f282a7210eedf10bc6a910b4ab59d91626d7289b
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-10-31 21:27:20 -0800

    🚀 UPDATE: updated env var

--- src/helpers/tokenHandler.ts
+++ src/helpers/tokenHandler.ts
@@ -1,19 +1,21 @@
 import { encrypt, decrypt } from ".";
 
+const APP_NAME = process.env.APP_NAME;
+
 export type Tokens = {
   accessToken: string;
   refreshToken: string;
 };
 
 export const getAccessToken = () => {
-  const encryptedToken = localStorage.getItem(`${process.env.APP_NAME}-stage`)!;
-  const tokens: Tokens = JSON.parse(decrypt(encryptedToken));
+  const encryptedToken = localStorage.getItem(`${APP_NAME}-stage`);
+  const tokens: Tokens = JSON.parse(decrypt(encryptedToken as string));
   return tokens.accessToken;
 };
 
 export const getTokenData = () => {
-  const encryptedToken = localStorage.getItem(`${process.env.APP_NAME}-stage`)!;
-  const tokens: Tokens = JSON.parse(decrypt(encryptedToken));
+  const encryptedToken = localStorage.getItem(`${APP_NAME}-stage`);
+  const tokens: Tokens = JSON.parse(decrypt(encryptedToken as string));
   return JSON.parse(
     Buffer.from(tokens.accessToken.split(".")[1], "base64").toString(),
   );
@@ -21,5 +23,5 @@ export const getTokenData = () => {
 
 export const encryptTokenData = (tokens: Tokens) => {
   const encrypted = encrypt(JSON.stringify(tokens));
-  localStorage.setItem(`${process.env.APP_NAME}-stage`, encrypted);
+  localStorage.setItem(`${APP_NAME}-stage`, encrypted);
 };
