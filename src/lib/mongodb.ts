b495b23faeec5f662e50dfecae5752d49d642348
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-11-25 19:51:19 -0800

    ✨ NEW: added mongodb lib

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = process.env.MONGODB_URI;
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+const globalWithMongo = global as typeof globalThis & {
+  _mongoClientPromise: Promise<any>;
+};
+
+if (process.env.NODE_ENV === "development") {
+  if (!globalWithMongo._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    globalWithMongo._mongoClientPromise = client.connect();
+  }
+  clientPromise = globalWithMongo._mongoClientPromise;
+} else {
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
