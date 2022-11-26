05f5b3d6a36c42a9c4a23babfa4374117a26f833
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-11-26 14:46:54 -0800

    ✨ NEW: added credentials

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -2,11 +2,11 @@ import { LiteralUnion, ClientSafeProvider } from "next-auth/react";
 import { BuiltInProviderType } from "next-auth/providers";
 
 declare module "next-auth/providers" {
-  type AvailableProviderType = "github" | "google";
+  type AvailableProviderType = "github" | "google" | "credentials";
 }
 
 declare module "next-auth/react" {
   interface AvailableSafeProvider extends ClientSafeProvider {
-    id: "github" | "google";
+    id: "github" | "google" | "credentials";
   }
 }
