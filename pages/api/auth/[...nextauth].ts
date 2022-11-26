b55c606a5bce3309c3d22072dc3c13cac95c843e
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-11-25 19:50:16 -0800

    ✨ NEW: added next auth route

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/signIn",
+  },
+  jwt: {
+    secret: process.env.JWT_KEY,
+  },
+  adapter: MongoDBAdapter(clientPromise),
+};
+
+export default NextAuth(authOptions);
