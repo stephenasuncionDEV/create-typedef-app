5b9378407a258346f661a85b2de6b44d2688d1b2
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-11-26 20:27:28 -0800

    🚀 UPDATE: connected credential provider to db

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,128 +1,22 @@
-import type { User } from "next-auth";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextAuthOptions, User, Account } from "next-auth";
+import type { JWTEncodeParams, JWTDecodeParams } from "next-auth/jwt";
 import type { AdapterUser } from "next-auth/adapters";
 import NextAuth from "next-auth";
+import { encode, decode } from "next-auth/jwt";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prismadb";
+import PrismaAdapter from "@/lib/prismaAdapter";
+import prisma from "@/lib/prismaDb";
 import { encrypt, decrypt } from "@/helpers/index";
-import { encode, decode } from "next-auth/jwt";
-
-export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-      async profile(profile, tokens) {
-        return Promise.resolve({
-          id: profile.id.toString(),
-          name: profile.name,
-          email: profile.email,
-          image: `https://ui-avatars.com/api/?name=${profile.name!.replaceAll(
-            " ",
-            "+",
-          )}`,
-        });
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID as string,
-      clientSecret: process.env.GOOGLE_SECRET as string,
-      async profile(profile, tokens) {
-        return Promise.resolve({
-          id: profile.id,
-          name: profile.name,
-          email: profile.email,
-          image: `https://ui-avatars.com/api/?name=${profile.name!.replaceAll(
-            " ",
-            "+",
-          )}`,
-        });
-      },
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        name: { label: "Name", type: "name", placeholder: "Name" },
-        email: { label: "Email", type: "email", placeholder: "Email address" },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials, req) {
-        try {
-          if (!credentials || !req.headers) return null;
-
-          const { name, email, password } = credentials;
-
-          // const client = await clientPromise;
-          // const db = client.db("create-typedef-template");
-          // let user = await db.collection("users").findOne({ email });
-
-          // if (req.headers.referer.indexOf("auth/login") !== -1) {
-          //   if (user && decrypt(user.password) !== password) return null;
-          // } else if (req.headers.referer.indexOf("auth/register") !== -1) {
-          //   if (!user) {
-          //     const newUser = await db.collection("users").insertOne({
-          //       name,
-          //       email,
-          //       password: encrypt(password),
-          //       image: `https://ui-avatars.com/api/?name=${name.replaceAll(
-          //         " ",
-          //         "+",
-          //       )}`,
-          //       emailVerified: null,
-          //     });
-
-          //     user = await db.collection("users").findOne({
-          //       email,
-          //     });
-
-          //     await db.collection("accounts").insertOne({
-          //       provider: "credentials",
-          //       type: "credential",
-          //       providerAcccountId: newUser.insertedId,
-          //       userId: newUser.insertedId,
-          //     });
-          //   }
-          // }
-
-          //console.log(user);
-
-          return null;
-        } catch (err: any) {
-          return null;
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth/login",
-    signUp: "/auth/register",
-    signOut: "/",
-  },
-  callbacks: {
-    async signIn({ user }: SignInCallback) {
-      console.log("signIn", user);
-      return true;
-    },
-    async session({ session, user }: SessionCallback) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
-      console.log("session", session);
-      return session;
-    },
-  },
-  adapter: PrismaAdapter(prisma),
-  debug: true,
-};
+import { setCookie, getCookie } from "cookies-next";
+import { randomUUID } from "crypto";
+import { uuid } from "uuidv4";
 
 export interface SignInCallback {
   user: User | AdapterUser;
+  account: Account | null;
 }
 
 export interface SessionCallback {
@@ -130,4 +24,182 @@ export interface SessionCallback {
   user: User | AdapterUser;
 }
 
-export default NextAuth(authOptions);
+export default function auth(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return NextAuth(req, res, {
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+        async profile(profile, tokens) {
+          return Promise.resolve({
+            id: profile.id.toString(),
+            name: profile.name,
+            email: profile.email,
+            image: `https://ui-avatars.com/api/?name=${profile.name!.replaceAll(
+              " ",
+              "+",
+            )}`,
+          });
+        },
+      }),
+      GoogleProvider({
+        clientId: process.env.GOOGLE_ID as string,
+        clientSecret: process.env.GOOGLE_SECRET as string,
+        async profile(profile, tokens) {
+          return Promise.resolve({
+            id: profile.id,
+            name: profile.name,
+            email: profile.email,
+            image: `https://ui-avatars.com/api/?name=${profile.name!.replaceAll(
+              " ",
+              "+",
+            )}`,
+          });
+        },
+      }),
+      CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          name: { label: "Name", type: "name", placeholder: "Name" },
+          email: {
+            label: "Email",
+            type: "email",
+            placeholder: "Email address",
+          },
+          password: {
+            label: "Password",
+            type: "password",
+            placeholder: "Password",
+          },
+        },
+        async authorize(credentials, req) {
+          if (!credentials || !req.headers) return null;
+
+          const { name, email, password } = credentials;
+
+          let user = await prisma.user.findUnique({
+            where: {
+              email,
+            },
+          });
+
+          if (req.headers.referer.indexOf("auth/login") !== -1) {
+            if (!user) throw new Error("User not found. Please sign up.");
+            if (decrypt(user.password!) !== password)
+              throw new Error("Invalid email or password.");
+          } else if (req.headers.referer.indexOf("auth/register") !== -1) {
+            if (user) throw new Error("Email was already used.");
+
+            user = await prisma.user.create({
+              data: {
+                name,
+                email,
+                image: `https://ui-avatars.com/api/?name=${name.replaceAll(
+                  " ",
+                  "+",
+                )}`,
+                password: encrypt(password),
+                emailVerified: null,
+              },
+            });
+
+            const newAccount = await prisma.account.create({
+              data: {
+                provider: "credentials",
+                type: "credential",
+                providerAccountId: user.id,
+                userId: user.id,
+              },
+            });
+          }
+
+          return user;
+        },
+      }),
+    ],
+    pages: {
+      signIn: "/auth/login",
+      signOut: "/",
+    },
+    session: {
+      strategy: "database",
+      maxAge: 30 * 24 * 60 * 60,
+      updateAge: 24 * 60 * 60,
+      generateSessionToken: () => {
+        return randomUUID() ?? uuid();
+      },
+    },
+    adapter: PrismaAdapter(prisma),
+    callbacks: {
+      async signIn({ user, account }: SignInCallback) {
+        if (!user || !account) return false;
+
+        if (
+          req.query.nextauth!.includes("callback") &&
+          req.query.nextauth!.includes("credentials") &&
+          req.method === "POST" &&
+          user
+        ) {
+          const sessionToken = randomUUID() ?? uuid();
+
+          let sessionExpiry = new Date();
+          sessionExpiry.setDate(sessionExpiry.getDate() + 30);
+
+          await PrismaAdapter(prisma).createSession({
+            sessionToken: sessionToken,
+            userId: user.id,
+            expires: sessionExpiry,
+          });
+
+          console.log("signIn", sessionToken, sessionExpiry);
+
+          setCookie("next-auth.session-token", sessionToken, {
+            req,
+            res,
+            expires: sessionExpiry,
+          });
+        }
+
+        return true;
+      },
+      async session({ session, user }: SessionCallback) {
+        console.log("session", session);
+        if (session.user) {
+          session.user.id = user.id;
+        }
+        return session;
+      },
+    },
+    jwt: {
+      async encode({ token, secret, maxAge }: JWTEncodeParams) {
+        if (
+          req.query.nextauth!.includes("callback") &&
+          req.query.nextauth!.includes("credentials") &&
+          req.method === "POST"
+        ) {
+          const cookie = getCookie("next-auth.session-token", { req, res });
+
+          if (cookie) return cookie as string;
+          return "";
+        }
+
+        return await encode({ token, secret, maxAge });
+      },
+      async decode({ token, secret }: JWTDecodeParams) {
+        if (
+          req.query.nextauth!.includes("callback") &&
+          req.query.nextauth!.includes("credentials") &&
+          req.method === "POST"
+        ) {
+          return null;
+        }
+
+        return await decode({ token, secret });
+      },
+    },
+    debug: true,
+  });
+}
