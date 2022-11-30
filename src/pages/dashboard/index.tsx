c0d7a1664dd10fceb7fc0a8ea79c2f9378d07fee
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-11-29 18:50:26 -0800

    🚀 UPDATE: updated dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,16 +1,22 @@
 import type { NextPage } from "next";
 import NextImage from "next/image";
 import { signOut } from "next-auth/react";
-import { Center, Button, Spinner, Text, Heading } from "@chakra-ui/react";
+import {
+  Center,
+  Button,
+  Spinner,
+  Text,
+  Heading,
+  Tag,
+  TagLabel,
+} from "@chakra-ui/react";
 import { useAuthData } from "@/hooks/auth/useAuthData";
 import Meta from "@/components/Meta";
 
 const Dashboard: NextPage = () => {
-  const { session, isLoading } = useAuthData();
+  const { session, isLoading, isUnAuthenticated } = useAuthData();
 
-  console.log(session);
-
-  if (isLoading) {
+  if (isLoading || isUnAuthenticated) {
     return (
       <Center as="main" flexDir="column" minH="100vh">
         <Spinner />
@@ -20,7 +26,7 @@ const Dashboard: NextPage = () => {
 
   return (
     <Center as="main" flexDir="column" minH="100vh">
-      <Meta title="404" />
+      <Meta title="Dashboard" />
       <Center flexDir="column">
         <NextImage
           src="/assets/images/logo.svg"
@@ -31,9 +37,16 @@ const Dashboard: NextPage = () => {
           blurDataURL="https://via.placeholder.com/300/26"
           placeholder="blur"
         />
-        <Heading as="h1">Welcome, </Heading>
-        <Text fontSize="10pt">Thanks for using create-typedef-app</Text>
-        <Button variant="primary" onClick={() => signOut()}>
+        <Heading as="h1" fontSize="22pt">
+          Welcome {session?.user?.name},{" "}
+        </Heading>
+        <Text fontSize="10pt" variant="subtle">
+          Thanks for using create-typedef-app
+        </Text>
+        <Tag mt="1em" size="sm">
+          <TagLabel>{session?.user?.email}</TagLabel>
+        </Tag>
+        <Button variant="primary" onClick={() => signOut()} size="sm" mt="2em">
           Logout
         </Button>
       </Center>
