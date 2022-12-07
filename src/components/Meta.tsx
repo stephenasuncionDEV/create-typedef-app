4dc0ae5be946213d989daf40fc17399dff8f464b
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-12-06 20:45:58 -0800

    ✨ NEW: added support for all logo sizes

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -16,20 +16,53 @@ const Meta: FC<MetaProps> = ({ title }) => {
       <meta name="author" content="YOUR_NAME" />
       <meta name="robots" content="index, follow" />
       <meta name="language" content="en" />
-      <meta name="theme-color" content="#348CD4" />
+      <meta name="theme-color" content="#763FE5" />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="SITE_URL" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content="DESCRIPTION" />
-      <meta property="og:image" content="/assets/logo.png" />
+      <meta property="og:image" content="/assets/images/logo.png" />
       <meta property="og:site_name" content="SITE_NAME" />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="SITE_URL" />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content="DESCRIPTION" />
-      <meta property="twitter:image" content="/assets/logo.png" />
+      <meta property="twitter:image" content="/assets/images/logo.png" />
+
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+
+      <link rel="manifest" href="/manifest.webmanifest" />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="152x152"
+        href="/assets/images/icon-152x152.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="144x144"
+        href="/assets/images/icon-144x144.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="120x120"
+        href="/assets/images/icon-120x120.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="114x114"
+        href="/assets/images/icon-114x114.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        sizes="72x72"
+        href="/assets/images/icon-72x72.png"
+      />
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/assets/images/icon-57x57.png"
+      />
     </Head>
   );
 };
