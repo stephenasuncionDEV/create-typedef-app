7a99d400b5fbd9bcf32dd509c92e842e888361ad
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-12-07 20:50:33 -0800

    🚀 UPDATE: renamed from useAuthData to useAuth

--- src/hooks/auth/useAuth.ts
+++ src/hooks/auth/useAuth.ts
@@ -1,5 +1,5 @@
-import { useSession } from "next-auth/react";
 import type { Session } from "next-auth";
+import { useSession } from "next-auth/react";
 
 export interface AuthDataResult {
   session: Session | null;
@@ -8,7 +8,7 @@ export interface AuthDataResult {
   isLoading: boolean;
 }
 
-export const useAuthData = (): AuthDataResult => {
+export const useAuth = (): AuthDataResult => {
   const { data: session, status } = useSession();
   const isAuthenticated = status === "authenticated";
   const isUnAuthenticated = status === "unauthenticated";
