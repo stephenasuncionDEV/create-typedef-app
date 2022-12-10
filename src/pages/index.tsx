255a938919b11f64e6dcb453435d764c740129d1
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-12-09 18:59:54 -0800

    🚀 UPDATE: updated landing style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,22 @@
 import type { NextPage } from "next";
 import NextLink from "next/link";
-import { Heading, Center, Button, VStack } from "@chakra-ui/react";
+import { Heading, Center, Button, VStack, Text } from "@chakra-ui/react";
 import Meta from "@/components/Meta";
 
 const Home: NextPage = () => {
   return (
     <Center as="main" flexDir="column" minH="100vh">
       <Meta title="Website" />
-      <VStack spacing="2em" className="backlight" borderRadius="10px" p="2em">
+      <VStack spacing="2em" borderRadius="10px" p="2em">
         <Heading as="h1">
           create-
-          <span style={{ color: "rgb(117,63,229)" }}>typedef</span>
+          <Text
+            as="span"
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+            bgClip="text"
+          >
+            typedef
+          </Text>
           -app
         </Heading>
         <NextLink href="/auth/login" passHref>
