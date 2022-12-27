7fc9c1612f573dbe82ffc181380efe1f770876c5
stephenasuncionDEV <stephenasuncion02@gmail.com>
2022-12-27 15:32:39 -0800

    🚀 UPDATE: improved code and support for regex

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,48 +11,65 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const { pathname } = req.nextUrl;
 
-  if (
-    pathname.startsWith("/_next") ||
-    pathname.startsWith("/api") ||
-    pathname.startsWith("/static") ||
-    /\.(.*)$/.test(pathname)
-  )
+  const apiRouteRegex = /^\/(?:_next|api|static|\.(?:.*))$/;
+  if (apiRouteRegex.test(pathname)) {
     return res;
+  }
+
+  const routeBehaviors = new Map<string | RegExp, "secured" | "repel">([
+    ["/dashboard", "secured"],
+    [/^\/auth/, "repel"],
+  ]);
+
+  let behavior: "secured" | "repel" | undefined;
+  for (const [route, routeBehavior] of Array.from(routeBehaviors.entries())) {
+    if (typeof route === "string" && pathname === route) {
+      behavior = routeBehavior;
+      break;
+    } else if (route instanceof RegExp && route.test(pathname)) {
+      behavior = routeBehavior;
+      break;
+    }
+  }
 
   const authCookie = req.cookies.get("next-auth.session-token") as
     | SessionCookie
     | undefined;
 
-  const securedRoutes = ["/dashboard"];
-  const repelRoutes = ["/auth"];
-
   let isVerified = false;
 
   if (authCookie) {
     const { value: sessionToken } = authCookie as SessionCookie;
-
-    const session = await fetch(`${config.clientURL}/api/auth/verifySession`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ sessionToken }),
-    });
-
-    const sessionData = (await session.json()) as Session;
-    if (!sessionData) {
-      return NextResponse.redirect(new URL("/auth/login", req.url));
-    }
-
-    isVerified = sessionToken === sessionData.sessionToken;
+    isVerified = await verifySession(sessionToken);
   }
 
-  if (repelRoutes.includes(pathname) && authCookie && isVerified) {
+  if (behavior === "repel" && authCookie && isVerified) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
-  } else if (securedRoutes.includes(pathname) && !isVerified) {
+  } else if (behavior === "secured" && !isVerified) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
   return res;
 }
+
+const verifySession = async (sessionToken: string): Promise<boolean> => {
+  const session = await fetch(`${config.clientURL}/api/auth/verifySession`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ sessionToken }),
+  });
+
+  if (!session.ok) {
+    return false;
+  }
+
+  const sessionData = (await session.json()) as Session;
+  if (!sessionData) {
+    return false;
+  }
+
+  return sessionToken === sessionData.sessionToken;
+};
