c7761037a45ec97303bcdca5423832206483c6b1
stephenasuncionDEV <stephenasuncion02@gmail.com>
2023-01-22 23:22:56 -0800

    ✨ NEW: added posthog analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,42 @@
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { trpc } from "@/server/trpc";
 import store from "@/store/index";
 import theme from "@/styles/theme/index";
+import posthog from "posthog-js";
 import "@/styles/globals.scss";
 
 const MyApp = ({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!process.env.POSTHOG_TOKEN) return;
+
+    posthog.init(process.env.POSTHOG_TOKEN, {
+      api_host: "https://app.posthog.com",
+    });
+
+    const handleRouteChange = (page: string) => {
+      posthog.capture("$pageview", {
+        page,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <ReduxProvider store={store}>
