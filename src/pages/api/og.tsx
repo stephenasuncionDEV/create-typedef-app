7755fbb8675eda6c8914e4c4e372e8a650597cfd
stephenasuncionDEV <stephenasuncion02@gmail.com>
2023-02-06 15:08:01 -0800

    🚀 UPDATE: removed experimental keyword

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 import { ImageResponse } from "@vercel/og";
 
 export const config = {
-  runtime: "experimental-edge",
+  runtime: "edge",
 };
 
 export default function handler(req: NextRequest) {
